a780b3024101603929e892e9cd232f8d88a3340b
Aviya Yechezkel <aviyay@edu.acc.ac.il>
2026-04-23 23:06:06 +0300

    Initial commit

---
+++ src/main/java/server_2026_b/server/controllers/UserController.java
@@ -0,0 +1,36 @@
+package server_2026_b.server.controllers;
+
+import server_2026_b.server.entities.User;
+import server_2026_b.server.responses.BasicResponse;
+import server_2026_b.server.service.Persist;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final Persist persist;
+
+    public UserController(Persist persist) {
+        this.persist = persist;
+    }
+
+    @PostMapping
+    public BasicResponse createUser(@RequestBody User user) {
+        persist.save(user);
+        return new BasicResponse(true, 0);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return persist.loadList(User.class);
+    }
+
+
+}
