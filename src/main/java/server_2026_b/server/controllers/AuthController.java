74701a483da2e728e98bca4dc2f00e70e7455ecf
Arhises <roma1039@gmail.com>
2026-04-27 18:24:44 +0300

    Created by: Shimon Haim & Diana Brook
    Implement authentication and token management for secure access to the application.
    This includes creating an AuthController for handling login requests,
    defining an AuthToken entity for storing authentication tokens,
    and implementing a TokenService for generating and validating tokens.
    Additionally, update the ResponseDto to include token information in the response.

--- src/main/java/server_2026_b/server/controllers/AuthController.java
+++ src/main/java/server_2026_b/server/controllers/AuthController.java
@@ -2,8 +2,11 @@ package server_2026_b.server.controllers;
 
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server_2026_b.server.requests.LoginRequest;
+import server_2026_b.server.responses.ResponseDto;
 
 @RestController
 @RequestMapping("/auth")
@@ -14,7 +17,10 @@ public class AuthController {
 //
 //
 //
-//    @PostMapping("/employerLogin")
+    @PostMapping("/loginEmployer")
+    public ResponseDto loginEmployer(@RequestBody LoginRequest request){
+        return authService.loginEmployer(request.getUsername(), request.getPassword());
+    }
 
 
 }
