a708688b109fc14d878c72450829f03792a08e23
Arhises <roma1039@gmail.com>
2026-04-28 20:08:10 +0300

    WorkDay feature init
    02 xd

--- src/main/java/server_2026_b/server/controllers/WorkDayController.java
+++ src/main/java/server_2026_b/server/controllers/WorkDayController.java
@@ -1,4 +1,10 @@
 package server_2026_b.server.controllers;
 
 public class WorkDayController {
+    // TODO: endpoints:
+    //	- enter { זמן, תאריך, מיקום, טוקן } => BasicResponse
+    //	- exit { זמן, תאריך, מיקום, תוקן } => BasicResponse
+    //	- lastEnter { טוקן } => זמן כניסה אחרון ללא יציאה
+    //                          BasicResponse + Date
+    //	- getAllWorkList { טוקן } => List<WorkDay>
 }
--- src/main/java/server_2026_b/server/entities/WorkDay.java
+++ src/main/java/server_2026_b/server/entities/WorkDay.java
@@ -1,4 +1,8 @@
 package server_2026_b.server.entities;
 
 public class WorkDay {
+    // TODO: לקשר ל-DB
+    //	שדות: id, user_id, enter_time, exite, time, location
+    //  בהמשך נרצה את השדה של WorkingSite במקום location
+    //	הגדרת השדות ומיפוי ב-objects.hbm.xml.
 }
