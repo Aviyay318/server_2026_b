66b529a52ee0f5ccc08cb39a44232fb609a5aa6c
Arhises <roma1039@gmail.com>
2026-04-28 12:45:08 +0300

    Complete authentication system
    Created by Anna Fyodorov & Eva Yonin:
    1. adjusted AuthController to use AuthService for authentication
    2. added Employee entity to represent users in the system
    3. created AuthService to handle authentication logic
    4. updated Persist to include methods for retrieving Employee data
    5. added Errors utility class for handling authentication errors
    
    Co-authored-by: Anna <anna.fyodorov@gmail.com>
    Co-authored-by: Eva <eva.yonin@gmail.com>

--- src/main/java/server_2026_b/server/controllers/AuthController.java
+++ src/main/java/server_2026_b/server/controllers/AuthController.java
@@ -7,18 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import server_2026_b.server.requests.LoginRequest;
 import server_2026_b.server.responses.ResponseDto;
+import server_2026_b.server.service.AuthService;
+import server_2026_b.server.utils.GenerateHash;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
 
-//    @PostMapping("/employeeLogin")
-//
-//
-//
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/loginEmployee")
+    public ResponseDto loginEmployee(@RequestBody LoginRequest request) {
+        // סיסמה לרישום עובדים
+//        String hashedPassword = GenerateHash.hashMd5(request.getUsername(), request.getPassword());
+//        System.out.println("the password: " + hashedPassword);
+        return authService.loginEmployee(request.getUsername(), request.getPassword());
+    }
+
     @PostMapping("/loginEmployer")
     public ResponseDto loginEmployer(@RequestBody LoginRequest request){
+        // סיסמה לרישום מנהלים
+//        String hashedPassword = GenerateHash.hashMd5(request.getUsername(), request.getPassword());
+//        System.out.println("the password: " + hashedPassword);
         return authService.loginEmployer(request.getUsername(), request.getPassword());
     }
 
