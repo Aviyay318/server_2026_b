552a265c38b368db3dd313af4ebb5909eb1df6dd
Shimon98 <lagziel.98@gmail.com>
2026-05-14 17:39:49 +0300

    Refactor auth service to use generic user login

--- src/main/java/server_2026_b/server/service/AuthService.java
+++ src/main/java/server_2026_b/server/service/AuthService.java
@@ -1,13 +1,9 @@
 package server_2026_b.server.service;
 
 import org.springframework.stereotype.Service;
-import server_2026_b.server.entities.RefreshToken;
-import server_2026_b.server.entities.Employee;
-import server_2026_b.server.entities.Employer;
 import server_2026_b.server.entities.User;
 import server_2026_b.server.responses.BasicResponse;
 import server_2026_b.server.responses.LoginResponse;
-import server_2026_b.server.utils.Errors;
 import server_2026_b.server.utils.GenerateHash;
 import server_2026_b.server.utils.TokenUtils;
 import server_2026_b.server.utils.UserType;
@@ -16,15 +12,13 @@ import static server_2026_b.server.utils.Errors.*;
 
 
 @Service
-public class AuthService { // מחוברת לדאטה בייס ע״י Persist
-    private final Persist persist;
+public class AuthService {
     private final TokenService tokenService;
     private final UserService userService;
     private final TokenExpirySseService tokenExpirySseService;
 
-    public AuthService(Persist persist, TokenService tokenService, UserService userService,
+    public AuthService( TokenService tokenService, UserService userService,
                        TokenExpirySseService tokenExpirySseService) {
-        this.persist = persist;
         this.tokenService = tokenService;
         this.userService = userService;
         this.tokenExpirySseService = tokenExpirySseService;
